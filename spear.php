666738cd92b7820d3ec6f39d0f9d82de41dac8f6
XGG <zapperhatz26@gmail.com>
2020-01-28 19:56:03 +0800

    OH AYAN COMMIT

--- spear.php
+++ spear.php
@@ -7,13 +7,13 @@
 echo "[ ~ ] Checking for existing scripts" . PHP_EOL;
 $out = shell_exec("cat ~/.bashrc | grep wahupdate");
 $out = explode(" ",$out);
-if($out[0]=="alias"){
-    echo "[ x ] WAH Assisted Update script found, checking for updates." . PHP_EOL;
-    echo $a = file_get_contents("http://localhost/script_final");
+if($out[1]=="'wahupdate'='sudo"){
+    echo "[ * ] WAH Assisted Update script found, checking for updates." . PHP_EOL;
+    // echo $a = file_get_contents("http://localhost/script_final");
     
 }else{
     echo "[ x ] WAH Assisted Update script not found, attempting to install script." . PHP_EOL;
     
-    echo $a = shell_exec("curl http://localhost/script_final > yeet");
+    // echo $a = shell_exec("curl http://localhost/script_final > yeet");
     shell_exec("echo 0 > .blood");
 }
