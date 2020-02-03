dd5dbb0acb99b04108dbbce2d7b6a4a40643a30c
XGG <zapperhatz26@gmail.com>
2020-02-03 12:04:41 +0800

    added flags and improvements  to scripts

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,7 +1,34 @@
-echo "Please type server password.."
-sleep 2
-echo "Initializing Autobackup System..."
-sleep 3
+
+#!/bin/bash
+echo "[ ~ ] Creating backup of current database"
 sh /var/tmp/backup.sh
-sleep 2
-sudo sh ~/Downloads/Auto/bak.sh
+
+
+echo "[ ~ ] Initializing Access Parameters to remote update server"
+git config --global user.email "wahph@wah.ph"
+git config --global user.name "wahph"
+cd /var/www/html/wah2
+
+git config credential.helper store
+echo "[ ~ ] Attempting to fix code conflicts"
+
+echo > composer.lock
+git add .
+git commit -m "purge composer.lock"
+
+echo '********************************'
+echo "[ ~ ] Updating server..."
+echo '********************************'
+
+git fetch http://wah4update:wah4update2019@gitlab.wah.ph/wahdev/wah2.git misuwahv3
+git checkout misuwahv3
+composer dump-autoload
+php artisan migrate
+php artisan clear-compile
+php artisan optimize
+php artisan cache:clear
+
+
+echo '********************************'
+echo '[ ~ ] Finalizing updates'
+sed -i 's/119.92.200.11/eclaimslive2.philhealth.gov.ph/g' /var/www/html/wah2/.env
--- bak.sh
+++ bak.sh
@@ -1,43 +1,26 @@
 #!/bin/bash
 
-eval git config --global user.email "wahph@wah.ph"
-sleep 1
-eval git config --global user.name "wahph"
-sleep 1
+git config --global user.email "wahph@wah.ph"
+git config --global user.name "wahph"
 cd /var/www/html/wah2
 echo "updating Misuwah Server. . ."
-sleep 3
 echo "Please type wah credential. . ."
-sleep 2
-eval git config credential.helper store
-sleep 3
-eval git checkout -- .
-sleep 3
-eval git checkout -b m3-updates
-sleep 3
-eval git pull origin m3-updates
+git config credential.helper store
+git checkout -- .
+git checkout -b m3-updates
+git pull origin m3-updates
 echo "Update Complete"
-sleep 2
 echo "Migrating Database. . . ."
-eval php artisan migrate
-sleep 2
-eval composer dump-autoload
-sleep 2
-eval php artisan clear-compile
-sleep 2
-eval php artisan optimize
-sleep 2
-eval php artisan cache:clear
-sleep 2
+php artisan migrate
+composer dump-autoload
+php artisan clear-compile
+php artisan optimize
+php artisan cache:clear
 echo "Updating to MISUWAH V3.0 is now complete!"
-sleep 2
 sed -i 's/119.92.200.11/eclaimslive2.philhealth.gov.ph/g' /var/www/html/wah2/.env
-sleep 3
-eval rm -r ~/Downloads/Auto
-sleep 3
-eval rm -r ~/Downloads/Misuwah3.0.zip
+rm -r ~/Downloads/Auto
+rm -r ~/Downloads/Misuwah3.0.zip
 echo "Thank You!"
-sleep 3
 
 
 
