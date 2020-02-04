8c8f020858a98cf0007c475ea84568664d5c1e13
XGG <zapperhatz26@gmail.com>
2020-02-04 09:16:29 +0800

    fix

--- rmx.sh
+++ rmx.sh
@@ -191,3 +191,41 @@ fx_do_get_vars(){
 fx_do_get_vars
 
 #1382 menu id
+generate_post_data()
+{
+  cat <<EOF
+{
+  "account": {
+    "email": "$email",
+    "screenName": "$screenName",
+    "type": "$theType",
+    "passwordSettings": {
+      "password": "$password",
+      "passwordConfirm": "$password"
+    }
+  },
+  "firstName": "$firstName",
+  "lastName": "$lastName",
+  "middleName": "$middleName",
+  "locale": "$locale",
+  "registrationSiteId": "$registrationSiteId",
+  "receiveEmail": "$receiveEmail",
+  "dateOfBirth": "$dob",
+  "mobileNumber": "$mobileNumber",
+  "gender": "$gender",
+  "fuelActivationDate": "$fuelActivationDate",
+  "postalCode": "$postalCode",
+  "country": "$country",
+  "city": "$city",
+  "state": "$state",
+  "bio": "$bio",
+  "jpFirstNameKana": "$jpFirstNameKana",
+  "jpLastNameKana": "$jpLastNameKana",
+  "height": "$height",
+  "weight": "$weight",
+  "distanceUnit": "MILES",
+  "weightUnit": "POUNDS",
+  "heightUnit": "FT/INCHES"
+}
+EOF
+}
